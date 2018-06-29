de4e45e1a63b09c5e88d13c5a7e15326578160c3
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-30 00:43:39 +0700

    change field 'content' in table 'news' from string to text

---
+++ db/migrations/20180629173956_change_content_field_to_text.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class ChangeContentFieldToText extends AbstractMigration
+{
+    /**
+     * Migrate up.
+     */
+    public function up()
+    {
+        $news = $this->table('news');
+        $news->changeColumn('content','text')->save();
+    }
+
+    /**
+     * Migrate down.
+     */
+    public function down()
+    {
+        $news = $this->table('news');
+        $news->changeColumn('content','string')->save();
+    }
+}
