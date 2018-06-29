b22ebafc11ae7aa925ac546f84d8aa66492bee65
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-29 22:32:08 +0700

    add models directory to autoloader

--- config/Loader.php
+++ config/Loader.php
@@ -8,11 +8,12 @@ class Loader
         $arr = explode('\\', $class);
         $prefix = array_shift($arr);
         //echo $prefix;
-        //todo add models directory
         if ($prefix == 'controllers') {
             $prefix_file = 'controllers/';
         } elseif ($prefix == 'config') {
             $prefix_file = 'config/';
+        } elseif ($prefix == 'models') {
+            $prefix_file = 'models/';
         }
         $file = $prefix_file . implode('/', $arr) . '.php';
         //echo $file;
