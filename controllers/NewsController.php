33bd024800abdebc3e1e290a0f3cbec295c1fa3d
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-30 14:40:42 +0700

    add methods index() for get all news, show() for get 1 new item, showCreateForm() for show view createForm, create() for create new item

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -2,18 +2,57 @@
 
 namespace controllers;
 
+use models\News;
+
 class NewsController
 {
+    private $newsModel;
+    /**
+     * NewsController constructor.
+     */
+    public function __construct()
+    {
+        $this->newsModel = new News();
+    }
+
     public function index()
     {
-        echo 'index';
+        $newsList = $this->newsModel->getAllNews();
+        require_once $_SERVER['DOCUMENT_ROOT'].'/views/index.php';
         return true;
     }
 
     public function show($id)
     {
-        echo $id;
-        echo 'show';
+        $itemNew = $this->newsModel->getNewById($id);
+        require_once $_SERVER['DOCUMENT_ROOT'].'/views/itemNew.php';
+        return true;
+    }
+
+    public function showCreateForm()
+    {
+        require_once $_SERVER['DOCUMENT_ROOT'].'/views/createForm.php';
+        return true;
+    }
+
+    public function create()
+    {
+//        $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/images/';
+//        $uploadfile = $uploaddir . basename($_FILES['preview_image']['name']);
+////        echo  $uploadfile;
+////        die();
+//        var_dump($uploadfile);
+//        die();
+//        if (copy($_FILES['preview_image']['tmp_name'], $uploadfile)) {
+//            echo "Файл корректен и был успешно загружен.\n";
+//            die();
+//        } else {
+//            echo "Возможная атака с помощью файловой загрузки!\n";
+//        }
+//
+////        echo $_FILES['preview_image']['name'];
+////        die();
+        $itemNew = $this->newsModel->createNew($_POST['title'],$_POST['content'],$previewImage);
         return true;
     }
 }
