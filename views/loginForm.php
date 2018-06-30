59e91d5f52edd7127d54d95d7fd51c97b454004d
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-30 14:46:07 +0700

    create view 'loginForm'

---
+++ views/loginForm.php
@@ -0,0 +1,8 @@
+<form action="/login/authorize" method="post">
+    <p>Имя пользователя: <input type="text" name="login" /></p>
+    <p>Пароль: <input type="password" name="password" /></p>
+    <?php if(!empty($message)) { ?>
+        <p><?php echo $message; ?></p>
+    <?php } ?>
+    <p><input type="submit" value="Войти" /></p>
+</form>
