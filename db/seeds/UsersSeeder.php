3674945e735530541e656fa6fd5fd7cfbfb3aaa6
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-07-01 04:20:46 +0700

    create two users. with admin rights and without rights

---
+++ db/seeds/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UsersSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     * Write your database seeder using this method.
+     */
+    public function run()
+    {
+        $users = $this->table('users');
+        $users->truncate();
+
+        $data = [];
+            $data[] = [
+                'login'      => 'admin',
+                'password'      => md5('123456'),
+                'isAdmin' => 1
+            ];
+
+        $data[] = [
+            'login'      => 'mortal',
+            'password'      => md5('qwerty'),
+            'isAdmin' => 0
+        ];
+        $users->insert($data)->save();
+
+    }
+}
