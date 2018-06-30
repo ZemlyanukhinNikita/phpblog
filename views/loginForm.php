2e33aea9ed3beeb18000377e6a13878de7af88b2
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-07-01 03:35:24 +0700

    added session_start()

--- views/loginForm.php
+++ views/loginForm.php
@@ -1,9 +1,11 @@
-<?php require_once 'layouts/header.php'?>
+<?php session_start(); ?>
+<?php require_once 'layouts/header.php' ?>
+
 <form action="/login/authorize" method="post">
-    <p>Имя пользователя: <input type="text" name="login" /></p>
-    <p>Пароль: <input type="password" name="password" /></p>
-    <?php if(!empty($message)) { ?>
+    <p>Имя пользователя: <input type="text" name="login"/></p>
+    <p>Пароль: <input type="password" name="password"/></p>
+    <?php if (!empty($message)) { ?>
         <p><?php echo $message; ?></p>
     <?php } ?>
-    <p><input type="submit" value="Войти" /></p>
+    <p><input type="submit" value="Войти"/></p>
 </form>
