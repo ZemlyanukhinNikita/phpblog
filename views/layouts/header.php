7a91bb69ce9cdb4a4bc154b2eb4ad78681ea42da
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-07-01 03:32:09 +0700

    added the user's welcome and button logout

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,4 +1,7 @@
-﻿﻿<!DOCTYPE html>
+﻿<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);?>
+﻿<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -16,8 +19,14 @@
 <div class="header">
     <h1><a href="/">News</a></h1>
     <div class="login">
+        <?php if(isset($_SESSION['logged_user'])) { echo 'Вы вошли,как '. $_SESSION['logged_user']['login'].
+            '<form method="post" action="/logout">
+            <input type="submit" value="Выход"/>
+        </form>';}
+        else { ?>
         <form method="post" action="/login">
             <input type="submit" value="Войти"/>
         </form>
+        <?php } ?>
     </div>
 </div>
