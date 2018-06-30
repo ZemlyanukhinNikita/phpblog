a856221f7c92c6e0c2ca7da57f9ea838129c042d
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-30 16:00:42 +0700

    add uploading image

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -7,6 +7,7 @@ use models\News;
 class NewsController
 {
     private $newsModel;
+
     /**
      * NewsController constructor.
      */
@@ -18,41 +19,53 @@ class NewsController
     public function index()
     {
         $newsList = $this->newsModel->getAllNews();
-        require_once $_SERVER['DOCUMENT_ROOT'].'/views/index.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/views/index.php';
         return true;
     }
 
     public function show($id)
     {
         $itemNew = $this->newsModel->getNewById($id);
-        require_once $_SERVER['DOCUMENT_ROOT'].'/views/itemNew.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/views/itemNew.php';
         return true;
     }
 
     public function showCreateForm()
     {
-        require_once $_SERVER['DOCUMENT_ROOT'].'/views/createForm.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/views/createForm.php';
         return true;
+    }
+
+    private function imageUpload()
+    {
+        $imageName = $_FILES['preview_image']['name'];
+        if ($imageName) {
+
+            if ($_FILES['preview_image']['error'] == 0) {
+
+                $tmpName = $_FILES['preview_image']['tmp_name'];
+                $ext = pathinfo($imageName, PATHINFO_EXTENSION);
+                $hashName = md5($imageName) . '.' . $ext;
+                if (copy($tmpName, $_SERVER['DOCUMENT_ROOT'] . '/images/' . $hashName)) {
+                    return $hashName;
+                } else {
+                    return null;
+                }
+            }
+        }
     }
 
     public function create()
     {
-//        $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/images/';
-//        $uploadfile = $uploaddir . basename($_FILES['preview_image']['name']);
-////        echo  $uploadfile;
-////        die();
-//        var_dump($uploadfile);
-//        die();
-//        if (copy($_FILES['preview_image']['tmp_name'], $uploadfile)) {
-//            echo "Файл корректен и был успешно загружен.\n";
-//            die();
-//        } else {
-//            echo "Возможная атака с помощью файловой загрузки!\n";
-//        }
-//
-////        echo $_FILES['preview_image']['name'];
-////        die();
-        $itemNew = $this->newsModel->createNew($_POST['title'],$_POST['content'],$previewImage);
+        //todo check form fields, and validate image
+        $previewImage = null;
+        if ($_FILES['preview_image']['name']) {
+            $previewImage = $this->imageUpload();
+        }
+
+        $itemNew = $this->newsModel->createNew($_POST['title'], $_POST['content'], $previewImage);
+
+        header('Location:/');
         return true;
     }
 }
