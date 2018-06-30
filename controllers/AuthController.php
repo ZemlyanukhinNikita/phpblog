63365cffda8bbcd9456eebeb76e9a8c80e37b66a
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-07-01 03:26:05 +0700

    added methods authenticate(), authorize() and logout()

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,18 +3,65 @@
 namespace controllers;
 
 
+use models\User;
+
 class AuthController
 {
+    private $userModel;
+
+    /**
+     * AuthController constructor.
+     */
+    public function __construct()
+    {
+        $this->userModel = new User();
+    }
+
     public function showLoginForm()
     {
-        require_once $_SERVER['DOCUMENT_ROOT'].'/views/loginForm.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/views/loginForm.php';
+    }
+
+    private function authenticate($login, $password)
+    {
+        $user = $this->userModel->getUserByLogin($login);
+        $message = '';
+        if (!$user) {
+            $message = 'Неверный логин';
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/views/loginForm.php';
+        } elseif ($password != $user['password']) {
+            $message = 'Неверный пароль';
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/views/loginForm.php';
+        }
+
+        session_start();
+        $_SESSION['logged_user'] = $user;
         return true;
     }
 
     public function authorize()
     {
-        echo 'auth';
-        return true;
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        if (empty($login) || empty($password)) {
+            $message = 'Заполните обязательные поля';
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/views/loginForm.php';
+            return true;
+        }
+        $this->authenticate($login, $password);
+
+        header('Location:/');
+    }
+
+    public function logout()
+    {
+        session_start();
+        if(isset($_SESSION['logged_user'])){
+        unset($_SESSION['logged_user']);
+        session_destroy();
+        header('Location:/');
+        }
+        return null;
     }
 
 
