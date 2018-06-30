26e4fdba50dc5a5adae939e86be4f00bd2b07a60
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-06-30 18:39:31 +0700

    Added method editNew() to editing news

--- models/News.php
+++ models/News.php
@@ -71,6 +71,21 @@ class News
         $stmt->bindParam(3, $previewImage);
         $stmt->execute();
 
+        return $this->conn->lastInsertId();;
+    }
+
+    public function editNew($id, $title, $content, $previewImage)
+    {
+
+        $previewImage = $this->replacingEmptyStringWithNull($previewImage);
+
+        $stmt = $this->conn->prepare("UPDATE news set title=?, content=?, preview_image_slug=? where id=?");
+        $stmt->bindParam(1, $title);
+        $stmt->bindParam(2, $content);
+        $stmt->bindParam(3, $previewImage);
+        $stmt->bindParam(4, $id);
+        $stmt->execute();
+
         return true;
     }
 
