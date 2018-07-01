50203b15584b8ada0caee6029147c1aab3cb21fb
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-07-01 17:34:53 +0700

    added required fields title and content and file`s extensions

--- views/createForm.php
+++ views/createForm.php
@@ -1,13 +1,13 @@
 <?php require_once 'layouts/header.php' ?>
 <form action="/news/create" method="post" enctype="multipart/form-data">
-    <p>Заголовок новости: <input type="text" name="title"/></p>
+    <p>Заголовок новости: <input type="text" name="title" required/></p>
     <div class="textArea">
         <p>Текст: <textarea rows="10" name="content"></textarea></p>
     </div>
-    <p>Изображение: <input name="preview_image" type="file"/></p>
-
-    <?php if (!empty($message)) { ?>
-        <p><?php echo $message; ?></p>
+    <input type="hidden" name="max_size" value="1000000">
+    <p>Изображение: <input name="preview_image" type="file" accept=".svg, .jpg, .jpeg, .png, .gif, .bmp"/></p>
+    <?php if (!empty($errorMessage)) { ?>
+        <p><?php echo $errorMessage; ?></p>
     <?php } ?>
     <p><input type="submit" value="Добавить новость"/></p>
 </form>
--- views/editForm.php
+++ views/editForm.php
@@ -1,16 +1,16 @@
 <?php require_once 'layouts/header.php' ?>
 <form action="/news/edit/<?php echo $itemNew['id'] ?>" method="post" enctype="multipart/form-data">
-    <p>Заголовок новости: <input type="text" name="title" value="<?php echo $itemNew['title'] ?>"/></p>
+    <p>Заголовок новости: <input type="text" name="title" value="<?php echo $itemNew['title'] ?>" required/></p>
     <div class="textArea">
         <p>Текст: <textarea rows="10" cols="45" name="content"><?php echo $itemNew['content'] ?></textarea></p>
     </div>
     <?php if ($itemNew['preview_image_slug']) { ?>
         <img src="<?php echo $itemNew['preview_image_slug'] ?>" alt=""/>
     <?php } ?>
-    <p>Изображение: <input name="preview_image" type="file"/></p>
+    <p>Изображение: <input name="preview_image" type="file" accept=".svg, .jpg, .jpeg, .png, .gif, .bmp"/></p>
 
-    <?php if (!empty($message)) { ?>
-        <p><?php echo $message; ?></p>
+    <?php if (!empty($errorMessage)) { ?>
+        <p><?php echo $errorMessage; ?></p>
     <?php } ?>
     <p><input type="submit" value="Сохранить новость"/></p>
 </form>
