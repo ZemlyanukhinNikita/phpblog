75e6f740a88f8989e75db1edde2954d1c1ccabaf
ZemlyanukhinNikita <nakeg8@gmail.com>
2018-07-06 16:05:08 +0700

    changed top 10 news list

--- public/views/index.php
+++ public/views/index.php
@@ -21,26 +21,7 @@
     <div class="weekPosts">
         <span class="posts"><h3>Посты недели </h3></span>
         <ul>
-            <?php
-            //берем дату ровно неделю назад
-            $weekAgo = date("Y-m-d H:i:s", mktime(date('h'), date('i'), date('s'), date('m'), date('d') - 7, date('Y')));
-            //создаем новый массив новостей опубликованных за эту неделю
-            $newArrayList = [];
-            foreach ($newsList as $item) {
-                if ($item['created_at'] > $weekAgo)
-                    $newArrayList[] = $item;
-            }
-            //сортируем полученный массив, по полю 'просмотры'
-            usort($newArrayList, function ($a, $b) {
-                if ($a['views'] == $b['views']) {
-                    return 0;
-                }
-                return ($a['views'] < $b['views']) ? +1 : -1;
-            });
-            $i = 0;
-            //выводим 10 самых просматриваемых новостей за эту неделю
-            foreach ($newArrayList as $item): ?>
-                <?php if ($i++ == 10) break; ?>
+            <?php foreach ($topNewsList as $item): ?>
                 <div class="right">
                     <li><a href="/news/<?php echo $item['id'] ?>"> <?php echo $item['title'] ?></a></li>
                 </div>
